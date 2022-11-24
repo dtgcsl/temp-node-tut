e8b028564137d765c71e59ae6dadeecb026338c2
dtgcsl <dtg.csl@gmail.com>
2022-11-24 12:04:48 +0700

    first commit

---
+++ 2-globals.js
@@ -0,0 +1,12 @@
+// GLOBAL - NO WINDOW!!!
+
+// __dirname - path to current directory
+// _filename - file name
+// require   - function to use modules(Common JS)
+// module    - info about current modules(file)
+// process   - info about env when program is being executed
+
+console.log(__dirname);
+setInterval(() => {
+  console.log("hello world");
+}, 1000);
